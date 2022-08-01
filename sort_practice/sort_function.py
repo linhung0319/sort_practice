e8ac47d63f5b5f1095cf4a62905141590f36e18a
linhung0319 <linhung0319@gmail.com>
2022-08-01 18:20:13 +0100

    perf: Optimize the algorithm of bubble_sort
    The array is already in order from the index of
    last swap to the end. Use a flag to denote the
    index of the last swap. If the array is already
    in ascending order, the time complexity of bubble
    sort could be O(n).

--- sort_practice/sort_function.py
+++ sort_practice/sort_function.py
@@ -7,13 +7,16 @@ def compare(a, b, reverse=False):
         return a < b
 
 def bubble_sort(nums, key=lambda x: x, reverse=False):
-    last_swap = len(nums) - 1
-    while last_swap > 0:
-        ### swap the adjacent from index 1 to last_swap
-        for i in range(1, last_swap + 1):
+    disorder = len(nums) - 1
+    while disorder > 0:
+        ### list is disordered before the index of last_swap
+        last_swap = 0
+        ### swap the adjacent from index 1 to disorder
+        for i in range(1, disorder + 1):
             if compare(key(nums[i-1]), key(nums[i]), reverse=reverse):
                 nums[i-1], nums[i] = nums[i], nums[i-1]
-        last_swap -= 1
+                last_swap = i
+        disorder = last_swap - 1
 
 if __name__ == '__main__':
     nums = [5, -1, 4, 2, 2]
