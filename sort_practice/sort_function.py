6007c2a2debd478e3d381acdc0a56d372e49f0a7
linhung0319 <linhung0319@gmail.com>
2022-08-01 22:10:22 +0100

    feat: Add selection_sort to sort_function.py

--- sort_practice/sort_function.py
+++ sort_practice/sort_function.py
@@ -18,7 +18,23 @@ def bubble_sort(nums, key=lambda x: x, reverse=False):
                 last_swap = i
         disorder = last_swap - 1
 
+def selection_sort(nums, key=lambda x: x, reverse=False):
+    n = len(nums)
+    for i in range(n):
+        ### iterate through the unsorted part of
+        ### array to find the minimum value
+        min_i, min_v = i, nums[i] 
+        for j in range(i + 1, n):
+            if compare(key(min_v), key(nums[j]), reverse):
+                min_v = nums[j]
+                min_i = j
+        ### swap the minimum value with the first value of
+        ### to place the minimum in front of the unsorted array
+        nums[i], nums[min_i] = nums[min_i], nums[i]    
+        
+
 if __name__ == '__main__':
     nums = [5, -1, 4, 2, 2]
-    bubble_sort(nums)
+    #bubble_sort(nums)
+    selection_sort(nums)
     print(nums)
