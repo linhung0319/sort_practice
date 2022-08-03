240569207b25b9868ae6938127b77b481afa8e1d
linhung0319 <linhung0319@gmail.com>
2022-08-03 17:44:10 +0100

    feat: Add pop() to tree.Heap
    pop() can pop out the minimum value
    from the heap and keep the heap
    still in order.

--- sort_practice/tree.py
+++ sort_practice/tree.py
@@ -5,6 +5,9 @@ class Heap():
         self.__reverse = reverse
         self.heapify(nums)
 
+    def _swap(self, left, right):
+        self.__array[left], self.__array[right] = self.__array[right], self.__array[left]
+
     def heapify(self, nums):
         self.__array = []
         for x in nums:
@@ -25,11 +28,53 @@ class Heap():
                             self.__reverse ):
                 break
             ### If x is smaller than its parent, swap with its parent
-            self.__array[x_parent_i], self.__array[x_i] = self.__array[x_i], self.__array[x_parent_i]
+            self._swap(x_parent_i, x_i)
             x_i = x_parent_i
                 
     def pop(self):
-        pass
+        if not self.__array:
+            return None
+        ### When we delete the first element from
+        ### a complete binary tree, we pop the last
+        ### element x and place it at the first index
+        ### of the array to keep a complete binary tree
+        pop_v = self.__array[0]
+        self.__array[0] = self.__array.pop()
+        n = len(self.__array)
+        x_i = 0
+        ### Since x might not be in the heap order, we need to 
+        ### check its child node and adjust the position of x
+        ### to the correct place 
+        while x_i * 2 + 1 < n:
+            x_il = x_i * 2 + 1   ### left child node
+            x_ir = x_i * 2 + 2   ### right child node
+            ### Swap x with the left child node, if the left 
+            ### child node exists and has the smallest value. 
+            if x_ir < n and compare(self.__key(self.__array[x_il]), 
+                                    self.__key(self.__array[x_ir]),
+                                    self.__reverse):
+                if  compare(self.__key(self.__array[x_i]), 
+                            self.__key(self.__array[x_ir]),
+                            self.__reverse):
+                    self._swap(x_i, x_ir)
+                    x_i = x_ir
+                    continue
+                ### If x doesn't need to swap, it is in 
+                ### already in the correct position.
+                break
+
+            ### Swap x with the right child node, if the right 
+            ### child node exists and has the smallest value.
+            if compare(self.__key(self.__array[x_i]), 
+                       self.__key(self.__array[x_il]),
+                       self.__reverse):
+                self._swap(x_i, x_il)
+                x_i = x_il
+                continue  
+            ### If x doesn't need to swap, it is in 
+            ### already in the correct position. 
+            break
+        return pop_v
 
     def show(self):
         return self.__array
