57af9c302ce25e0f6d2c5bea13a280b1ac7514cb
linhung0319 <linhung0319@gmail.com>
2022-08-03 15:19:36 +0100

    test: Add a test for tree.Heap.pop()

--- tests/test_tree.py
+++ tests/test_tree.py
@@ -40,3 +40,15 @@ class Test_Heap(unittest.TestCase):
         max_heap = Heap(reverse=True)
         max_heap.heapify(x)
         self.assertEqual(max_heap.show(), [7, 5, 7, -1, 4, 6, -1])
+
+    def test_pop(self):
+        x = [-1, 7, 6, 5, 4, 7, -1]
+        min_heap = Heap(x)
+        self.assertEqual(min_heap.pop(), -1)
+        self.assertEqual(min_heap.pop(), -1)
+        self.assertEqual(min_heap.pop(), 4)
+
+        max_heap = Heap(x, reverse=True)
+        self.assertEqual(max_heap.pop(), 7)
+        self.assertEqual(max_heap.pop(), 7)
+        self.assertEqual(max_heap.pop(), 6)
