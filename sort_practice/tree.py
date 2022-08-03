0a807905163a8ec621a47e80f50191c54ff2279f
linhung0319 <linhung0319@gmail.com>
2022-08-03 09:28:32 +0100

    feat: Add push() in tree.Heap
    push(x) can push a new value into Heap
    in a manner that keep Heap in order.

--- sort_practice/tree.py
+++ sort_practice/tree.py
@@ -1,16 +1,34 @@
-
+from sort_practice.sort_function import compare
 class Heap():
     def __init__(self, key=lambda x: x, reverse=False):
-        pass
+        self.__array = []
+        self.__key = key
+        self.__reverse = reverse
 
     def heapify(self, x, key=lambda x: x, reverse=False):
         pass
 
     def push(self, x):
-        pass
+        self.__array.append(x)
+        x_i = len(self.__array) - 1
+
+        ### Stop adjusting the position of the pushed value
+        ### x if the pushed value is already at the root.
+        while x_i > 0:
+            x_parent_i = (x_i - 1) // 2
+            ### Stop adjusting the position of x, if 
+            ### x is already in the correct position
+            if not compare( self.__key(self.__array[x_parent_i]), 
+                            self.__key(self.__array[x_i]), 
+                            self.__reverse ):
+                break
+            ### If x is smaller than its parent, swap with its parent
+            self.__array[x_parent_i], self.__array[x_i] = self.__array[x_i], self.__array[x_parent_i]
+            x_i = x_parent_i
+                
 
     def pop(self):
         pass
 
     def show(self):
-        pass
+        return self.__array
